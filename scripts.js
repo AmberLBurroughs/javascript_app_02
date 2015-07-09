620f07d1fe318ea8c6bd0e407b7cb6303a2872d2
amber <amberlburroughs>
2015-07-09 16:46:52 +0100

    introducing variables, i created a variable then called the variable in a comman, then i changed the value of the variable

--- scripts.js
+++ scripts.js
@@ -4,3 +4,7 @@ alert(Can you get this message to appear?);
 document.write("<h2>My first JavaScript program</h2>");
 document.write("<p>I'm practicing 'debugging'.</p>");
 console.log("End Program");
+
+var message = "Hello!";
+alert(mesaage);
+message = "Welcome to JavaScript Basics.";
