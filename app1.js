2eabe0ba76001881b2a90a86fa3fd88d94991f80
amber <amberlburroughs>
2015-07-16 17:59:48 +0100

    playing with conditional statements

---
+++ app1.js
@@ -0,0 +1,8 @@
+//conditinal statement 
+
+var answer = prompt('what programming language is the name of a gem?');
+if ( answer === 'Ruby' ) {
+  document.write("<p> That's right!</p>");
+} else {
+  document.write("<p> Sorry, </p>" + answer "<p> is not the correct answer.</p>");
+}
