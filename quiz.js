cc1532e8591ceae29de7842013e53e7ef92707be
amber <amberlburroughs>
2015-08-03 14:35:37 -0700

    created a radom number guessing game and a quiz game

---
+++ quiz.js
@@ -0,0 +1,48 @@
+/*
+A simple 5 question quiz
+adds up how many questions are correct
+give a score according to how many are correct
+*/
+
+// 5 questions & a score of zero
+// test is questions 1-5 are correct adds 1 point to score 
+var score = 0;
+
+var answerOne = prompt('what color is a banana?');
+if( answerOne.toUpperCase() === 'YELLOW'){
+  score += 1;
+}
+
+var answerTwo = prompt('what is 5 * 100');
+if( answerTwo === '500'){
+  score += 1;
+}
+
+var answerThree = prompt('what color is the ocean?');
+if( answerThree.toUpperCase() === 'BLUE'){
+  score += 1;
+}
+var answerFour = prompt('what is 60 / 2');
+if( answerFour === '30'){
+  score += 1;
+}
+
+var answerFive = prompt('what day comes after Monday?');
+if ( answerFive.toUpperCase() === 'TUESDAY'){
+  score += 1;
+}
+
+
+document.write('<h3>You answered ' + score + ' questions correctly!</h3>');
+
+// according to what the end score is player is ranked 
+if (score === 5) {
+  document.write('<h2>You earned a <strong>gold crown</strong>.</h2>');
+}else if (score === 3 || score === 4 ) {
+  document.write('<h2>You earned a <strong>silver crown</strong>.</h2>');
+}else if (score === 1 || score === 2 ) {
+  document.write('<h2>You earned a <strong>bronze crown</strong>.</h2>');
+} else {
+  document.write('<h2>You did not answer any of the questions correctly :( </h2>');
+}
+ 
